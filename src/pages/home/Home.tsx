13e4378cb8114d3f2c18e875d291107c0e83c5f7
vbouillennec <val.bouillennec@gmail.com>
2020-05-14 11:25:50 +0200

    home-page: first version DONE

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
-import {Header} from 'react-native-elements'
-import TriangleBackground from '../../components/TriangleBackground';
+import {Header} from 'react-native-elements';
 
 export default function HomeView() {
+
+    function fakeList() {
+        let fakeList:Array<any> = []
+        for (let i = 0; i < 10; i++) {
+            fakeList.push(
+            <View key={i} style={styles.row_grid}>
+                <View style={styles.element_grid_left}/>
+                <View style={styles.element_grid_right}/>
+            </View>);
+        }
+
+        return fakeList;
+        
+    }
 
     return(
         <View style={{flex: 1}}>
@@ -16,22 +29,7 @@ export default function HomeView() {
             <SafeAreaView style={styles.container}>
                 <ScrollView style={styles.scrollView}>
                     <View style={styles.grid}>
-                        <View style={styles.row_grid}>
-                            <View style={styles.element_grid}/>
-                            <View style={styles.element_grid}/>
-                        </View>
-                        <View style={styles.row_grid}>
-                            <View style={styles.element_grid}/>
-                            <View style={styles.element_grid}/>
-                        </View>
-                        <View style={styles.row_grid}>
-                            <View style={styles.element_grid}/>
-                            <View style={styles.element_grid}/>
-                        </View>
-                        <View style={styles.row_grid}>
-                            <View style={styles.element_grid}/>
-                            <View style={styles.element_grid}/>
-                        </View>
+                        {fakeList()}
                     </View>
                 </ScrollView>
             </SafeAreaView>
@@ -52,17 +50,28 @@ const styles = StyleSheet.create({
     },
     grid: {
         flex: 1,
+        marginBottom: 8,
         flexDirection: "column"
     },
     row_grid: {
+        marginTop: 8,
         flex: 0.5,
         flexDirection: "row"
     },
-    element_grid: {
+    element_grid_right: {
         flex: 1,
         height: 300,
-        margin: 5,
-        backgroundColor: "#599688"
+        marginLeft: 4,
+        marginRight: 8,
+        backgroundColor: "#599688",
+        borderRadius: 8,
+    },
+    element_grid_left: {
+        flex: 1,
+        height: 300,
+        marginLeft: 8,
+        marginRight: 4,
+        backgroundColor: "#599688",
+        borderRadius: 8,
     }
-
 });
