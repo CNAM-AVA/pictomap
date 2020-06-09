d2a8cfa2475a9e87441255c0dee973b4c4adb436
Adrien NETO FERREIRA <iamnautilusjungle@gmail.com>
2020-06-09 13:51:07 +0200

    update user service

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -23,7 +23,6 @@ export default class UserService {
         })
     }
 
-
     user = new Observable<User>(new User({
         uuid: '',
         name: '',
@@ -40,37 +39,38 @@ export default class UserService {
         return new Promise((resolve, reject) => {
 
             auth.signInWithEmailAndPassword(credentials.email, credentials.password)
-            .then(res => {
-                let user = res.user!;
+                .then(res => {
+                    let user = res.user!;
 
-                console.log(user);
+                    console.log("test");
 
-                firestore.collection("users").doc(user.uid).get()
-                .then((doc) => {
+                    firestore.collection("users").doc(user.uid).get()
+                        .then((doc) => {
 
-                    if (!doc.exists)
-                        reject("No such document");
-                        
-                        
-                    let data = doc.data();
-                    this.user = new Observable<User>(new User({
-                        uuid: user.uid,
-                        name: user.displayName!,
-                        mail: user.email!,
-                        profile_picture: user.photoURL!,
-                        indiana_jones: data!.indiana_jones,
-                        created_at: data!.created_at,
-                        updated_at: data!.updated_at,
-                        is_authenticated: true
-                    }))
-                    resolve(this.user);
+                            if (!doc.exists)
+                                reject("No such document");
+
+
+                            let data = doc.data();
+                            this.user = new Observable<User>(new User({
+                                uuid: user.uid,
+                                name: data!.name,
+                                mail: user.email!,
+                                profile_picture: data!.profile_picture,
+                                indiana_jones: data!.indiana_jones,
+                                created_at: data!.created_at,
+                                updated_at: data!.updated_at,
+                                is_authenticated: true
+                            }))
+
+                            resolve(this.user);
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        })
+                }).catch(err => {
+                    reject(err);
                 })
-                .catch((err) => {
-                    console.log(err);
-                })
-            }).catch(err => {
-                reject(err);
-            })
         });
     }
 
@@ -102,6 +102,10 @@ export default class UserService {
     }
 
     isAuthenticated() {
-        return this.user.get().isAuthenticated();
+        return this.getUser().isAuthenticated();
+    }
+
+    getUser(): User {
+        return this.user.get();
     }
 }
